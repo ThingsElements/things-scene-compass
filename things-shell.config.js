565568fa83e2184df8e56819f6138cd6cf208ffa
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 16:36:41 +0900

    fix component template spec.

--- things-shell.config.js
+++ things-shell.config.js
@@ -1,27 +1,26 @@
 import locales from './locales';
 
+import icon from '../assets/compass.png';
+
 var templates = [{
-  name: 'Compass', /* 다국어 키 표현을 어떻게.. */
-  description: '...', /* 다국어 키 표현을 어떻게.. */
+  type: 'compass',
+  description: 'compass',
   group: 'etc', /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../', /* 또는, Object */
-  template: {
+  icon,
+  model: {
     type: 'compass',
-    model: {
-        type: 'compass',
-        cx: 150,
-        cy: 150,
-        rx: 50,
-        ry: 50,
-        value: 0,
-        fontSize: 20,
-        fontColoe: '#000',
-        fillStyle: '#F2F2F2',
-        strokeStyle: '#AAAAAA',
-        lineWidth: 10,
-        fontColor: '#ff0000',
-        alpha: 1
-    }
+    cx: 150,
+    cy: 150,
+    rx: 50,
+    ry: 50,
+    value: 0,
+    fontSize: 20,
+    fontColoe: '#000',
+    fillStyle: '#F2F2F2',
+    strokeStyle: '#AAAAAA',
+    lineWidth: 10,
+    fontColor: '#ff0000',
+    alpha: 1
   }
 }];
 
