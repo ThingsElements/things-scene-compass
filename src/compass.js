51d353640e876022c847237667f35aa523baf82e
EunHyeYun <lillyeh@hatiolab.com>
2020-01-10 12:27:41 +0900

    3D가 제공되지 않는 Component가 property-sidebar의 shape의 3D 입력창이 제공되고 있는 문제  EunHyeYun committed
    related to hatiolab/things-board#302

--- src/compass.js
+++ src/compass.js
@@ -2,40 +2,40 @@
  * Copyright © HatioLab Inc. All rights reserved.
  */
 
- import { Component, Ellipse } from '@hatiolab/things-scene'
+import { Component, Ellipse } from '@hatiolab/things-scene'
 
 const NATURE = {
   mutable: false,
   resizable: true,
   rotatable: true,
-  properties : [{
-    type: 'number',
-    label: 'value',
-    name: 'value',
-    property: 'value'
-  }]
+  properties: [
+    {
+      type: 'number',
+      label: 'value',
+      name: 'value',
+      property: 'value'
+    }
+  ]
 }
 
 export default class Compass extends Ellipse {
+  is3dish() {
+    return false
+  }
 
   _draw(context) {
-    var {
-      value = 0,
-      fillStyle,
-      cx, cy, rx, ry
-    } = this.model;
+    var { value = 0, fillStyle, cx, cy, rx, ry } = this.model
 
     context.translate(cx, cy)
 
     ////  메인 원 그리기  ////
     context.beginPath()
     context.ellipse(0, 0, Math.abs(rx), Math.abs(ry), 0, 0, 2 * Math.PI)
-    context.ellipse(0, 0, Math.abs(rx * 0.75), Math.abs(ry * 0.75), 0, 2 * Math.PI, 0, true)  // 반대로 그리며 원을 지움.
+    context.ellipse(0, 0, Math.abs(rx * 0.75), Math.abs(ry * 0.75), 0, 2 * Math.PI, 0, true) // 반대로 그리며 원을 지움.
 
     this.drawStroke(context)
     this.drawFill(context)
     context.closePath()
-
 
     ////  무늬 그리기  ////
     context.beginPath()
@@ -45,30 +45,28 @@ export default class Compass extends Ellipse {
     context.lineTo(0, -ry * 0.09)
 
     context.moveTo(0, ry * 0.65)
-    context.lineTo(rx * 0.09 , 0)
+    context.lineTo(rx * 0.09, 0)
     context.lineTo(0, -ry * 0.65)
-    context.lineTo(-rx * 0.09 , 0)
+    context.lineTo(-rx * 0.09, 0)
 
     context.fillStyle = fillStyle
     context.fill()
     context.closePath()
 
-
     ////  텍스트 그리기  ////
     context.beginPath()
     context.fillStyle = 'black'
-    context.font = (rx + ry) / 13 + "px arial"
-    context.textBaseline = "middle"
-    context.textAlign = "center"
-    context.fillText('N', 0, -ry + (ry * 0.125))
-    context.fillText('S', 0, ry - (ry * 0.125))
-    context.fillText('W', -rx + (rx * 0.125), 0)
-    context.fillText('E', rx - (rx * 0.125), 0)
-
+    context.font = (rx + ry) / 13 + 'px arial'
+    context.textBaseline = 'middle'
+    context.textAlign = 'center'
+    context.fillText('N', 0, -ry + ry * 0.125)
+    context.fillText('S', 0, ry - ry * 0.125)
+    context.fillText('W', -rx + rx * 0.125, 0)
+    context.fillText('E', rx - rx * 0.125, 0)
 
     ////  바늘 그리기  ////
     context.beginPath()
-    var ang = (value + (this._anim_alpha || 0)) / 50 * Math.PI
+    var ang = ((value + (this._anim_alpha || 0)) / 50) * Math.PI
     context.rotate(ang)
 
     context.moveTo(0, -ry * 0.65)
@@ -85,7 +83,6 @@ export default class Compass extends Ellipse {
     context.fill()
 
     context.rotate(-ang)
-
 
     ////  중앙 원 그리기  ////
     context.beginPath()
@@ -105,13 +102,12 @@ export default class Compass extends Ellipse {
     context.translate(-cx, -cy)
   }
 
-  _post_draw(context){
+  _post_draw(context) {
     this.drawText(context)
   }
 
   onchange(after, before) {
-    if(!after.hasOwnProperty('value'))
-      return
+    if (!after.hasOwnProperty('value')) return
 
     var self = this
     var diff = after.value - before.value
@@ -132,7 +128,7 @@ export default class Compass extends Ellipse {
     }).start()
   }
 
-  get nature(){
+  get nature() {
     return NATURE
   }
 }
